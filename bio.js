d6b39240fba095b1b20a30bea97aa22de4f70f05
Claudia Chan <claudiachan@claudia-air-weeeeeeee.local>
2022-06-08 09:48:17 +1000

    This is the first time we're using github to link our own stuff weeeeee

---
+++ bio.js
@@ -0,0 +1,12 @@
+// none of this works. lajsdklfjlajsdlfjasdjfa
+let button = document.getElementById("btnMaths");
+let div = document.getElementById("subjectDescText");
+
+button.addEventListener(
+    "click", function () {
+        div.innerHTML = "maths is pretty cool";
+        button.style.backgroundColor = "grey";
+    }
+)
+
+
