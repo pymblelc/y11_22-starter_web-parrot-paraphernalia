4024c7fb83536fce248b39f100f397d0dfbb5792
Claudia Chan <claudiachan@claudia-air-weeeeeeee.local>
2022-07-19 09:11:19 +1000

    css-ified and analyses things

--- assessment/exampleScript.js
+++ assessment/exampleScript.js
@@ -1,6 +1,6 @@
-
-
-submitBtn.addEventListener( "click", function () {
+document.getElementById("submit").addEventListener( // <button id="submit">Submit</button>
+    "click",
+    function () {
         let fileInput = document.getElementById("upload"); // <input type="file" id="upload">
         let blob = new Blob([fileInput.files[0]]);
         ImageAPI.analyseFacesBlob(
--- assessment/script.js
+++ assessment/script.js
@@ -4,39 +4,48 @@ let takePhotoBtn = document.getElementById("takePhoto");
 let myWebcam = document.getElementById("webcam");
 let myCanvas = document.getElementById("canvas");
 let submitBtn = document.getElementById("submit");
-// const snapSoundElement = document.getElementById('snapSound');
+let retakeBtn = document.getElementById("retake");
+let canvasCtx = myCanvas.getContext("2d"); // this turns the 2d canvas image into something that can be analysed, aka a 'context'
+// let snapSoundElement = document.getElementById('snapSound');
 
-let webcam = new Webcam(myWebcam, "user", myCanvas); // is this making a function
+let webcamClass = new Webcam(myWebcam, "user", myCanvas); // class is like a function but SPECIAL
 
 
 startCameraBtn.addEventListener("click", function() {
-    webcam.start()
+    webcamClass.start()
     .then((result) => {
         console.log("webcam started!");
     })
     .catch((error) => {
-        console.log("error D:");
+        console.log("error sadge");
     });
 })
 
 stopCameraBtn.addEventListener("click", function() {
-    webcam.stop();
+    webcamClass.stop();
 })
 
 takePhotoBtn.addEventListener("click", function () {
-    var picture = webcam.snap(); // what 
-    webcam.stop();
+    var picture = webcamClass.snap(); //what. why is it a variable
+    webcamClass.stop();
   });
 
-  submitBtn.addEventListener("click", function() {
-        let blob = new Blob([fileInput.files[0]]);
-        ImageAPI.analyseFacesBlob(blob,(data) => {
-                console.log(data);
-            }
-        );
-  })
+retakeBtn.addEventListener("click", function() {
+    webcamClass.start();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+})
+  // time to actually send this info to the api
+  // canvas -> getcontext ('canvasCtx') -> blob -> api
+submitBtn.addEventListener("click", function() {
+    myCanvas.toBlob(function(blob) {
+        console.log("blob-ified...")
+        ImageAPI.analyseImageBlob(blob, (data) => {
+          console.log(data);
+          let tags = data.description.tags;
+          tag
+        })
+    })
+})
 
 
-
-
-
+// 
